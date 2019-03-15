a1c3701b33dd0000e89e78287bfd7876d5c63cac
Rahim ghasemi <tak1.ghasemi@gmail.com>
2019-03-15 18:09:16 +0330

    image_compress
    one class in c# for compress size image

---
+++ imgcompres.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for imgcompres
+/// </summary>
+public class imgcompres
+{
+	public imgcompres()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public void ImageCompression(double scaleFactor, Stream sourcePath, string targetPath)
+    {
+        using (var image = System.Drawing.Image.FromStream(sourcePath))
+        {
+            var newWidth = (int)(image.Width * scaleFactor);
+            var newHeight = (int)(image.Height * scaleFactor);
+            var thumbnailImg = new Bitmap(newWidth, newHeight);
+            var thumbGraph = Graphics.FromImage(thumbnailImg);
+            thumbGraph.CompositingQuality = CompositingQuality.HighQuality;
+            thumbGraph.SmoothingMode = SmoothingMode.HighQuality;
+            thumbGraph.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            var imageRectangle = new Rectangle(0, 0, newWidth, newHeight);
+            thumbGraph.DrawImage(image, imageRectangle);
+            thumbnailImg.Save(targetPath, image.RawFormat);
+        }
+    }
+}
