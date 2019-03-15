2a4a7bc4c5dc0ad126701c4733c91b2781e396ac
Rahim ghasemi <tak1.ghasemi@gmail.com>
2019-03-15 17:28:48 +0330

    time class farsi
    This is a sampel for tutorial git

---
+++ time.cs
@@ -0,0 +1,48 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+/// <summary>
+/// Summary description for time
+/// </summary>
+public class time
+{
+    static string d2;
+		public string time1(DateTime s)
+        {
+            
+
+
+
+            PersianCalendar d = new PersianCalendar();
+
+            if (Convert.ToInt32(d.GetMonth(s)) > 9 && Convert.ToInt32(d.GetDayOfMonth(s)) > 9)
+            {
+                d2 = d.GetYear(s).ToString() + "/" + d.GetMonth(s).ToString() + "/" + d.GetDayOfMonth(s).ToString();
+                
+            }
+            else if (Convert.ToInt32(d.GetMonth(s)) <= 9 && Convert.ToInt32(d.GetDayOfMonth(s)) <= 9)
+            {
+                d2 = d.GetYear(s).ToString() + "/0" + d.GetMonth(s).ToString() + "/0" + d.GetDayOfMonth(s).ToString();
+                
+            }
+            if (Convert.ToInt32(d.GetDayOfMonth(s)) > 9 && Convert.ToInt32(d.GetMonth(s)) <= 9)
+            {
+                d2 = d.GetYear(s).ToString() + "/0" + d.GetMonth(s).ToString() + "/" + d.GetDayOfMonth(s).ToString();
+                
+            }
+            else if (Convert.ToInt32(d.GetDayOfMonth(s)) <= 9 && Convert.ToInt32(d.GetMonth(s)) > 9)
+            {
+                d2 = d.GetYear(s).ToString() + "/" + d.GetMonth(s).ToString() + "/0" + d.GetDayOfMonth(s).ToString();
+                
+            }
+
+            return d2;
+
+           
+
+        }
+	
+}
